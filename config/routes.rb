526b962f693753d0ef2cdf360ebd7c2ea9ea57f6
Marilyn Remigio <marilynremigio@gmail.com>
2023-03-29 17:42:39 -0600

    add new controller, new blog form, completes story 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/blogs' => 'blog#index', as: 'blogs'
+  get '/blogs/new' => 'blog#new', as: 'new_blog'
   get '/blogs/:id' => 'blog#show', as: 'blog'
+  
 end
