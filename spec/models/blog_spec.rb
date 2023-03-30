c0aeb793b8c3e9592f63bff421720258ff23e888
csaument <c.s.aument@gmail.com>
2023-03-30 10:23:55 -0700

    creates validation for records and tests validation in rspec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without valid attributes' do
+      blog = Blog.create title: 'Testing Blogs!', content: 'Each post must meet a strict standard, surely!'
+      expect(blog).to be_valid
+  end
+
+  it 'title must be present' do
+      blog = Blog.create content: 'Each post must meet a strict standard, surely!'
+      expect(blog.errors[:title]).to_not be_empty
+  end
+
+  it 'content must be present' do
+      blog = Blog.create title: 'Testing Blogs!'
+      expect(blog.errors[:content]).to_not be_empty
+  end
+
+  it 'title must be 4 characters' do
+      blog = Blog.create title: 'Tes', content: 'Each post must meet a strict standard, surely!'
+      expect(blog.errors[:title]).to_not be_empty
+  end
+
+  it 'content must be 10 characters' do
+      blog = Blog.create title: 'Testing Blogs!', content: 'Each'
+      expect(blog.errors[:content]).to_not be_empty
+  end
+
+  it 'title must be unique' do
+      Blog.create title: 'Testing Blogs!', content: 'This should post!'
+      blog = Blog.create title: 'Testing Blogs!', content: 'This should not!'
+      expect(blog.errors[:title]).to_not be_empty
+  end
 end
